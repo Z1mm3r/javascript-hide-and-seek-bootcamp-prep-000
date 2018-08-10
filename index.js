b51f3502a04a6603745e916559759dc7f5ab27a1
Casey Zimmer <czimmer@ramapo.edu>
2018-08-10 23:13:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector);
+}
 
+function nestedTarget()
+{
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n)
+{
+  var selectorList = document.getElementById("app").querySelectorAll(".ranked-List li");
+  
+  console.log(selectorList);
+  
+  for (let i = 0; i < selectorList.length; i++)
+  {
+    selectorList[i].innerHTML = (praseInt(selectorList[i].innerHTML,10) + n).toString();
+  }
+}
