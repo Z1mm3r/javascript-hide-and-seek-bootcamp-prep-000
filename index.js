92ff24e7d2ec91766734f8d3b07f8c5c85330c3d
Casey Zimmer <czimmer@ramapo.edu>
2018-08-11 00:47:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector);
+}
 
+function nestedTarget()
+{
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n)
+{
+  var selectorList = document.querySelectorAll("#app ul.ranked-list li");
+  
+  for (let i = 0; i < selectorList.length; i++)
+  {
+    selectorList[i].innerHTML = (parseInt(selectorList[i].innerHTML,10) + n).toString();
+  }
+}
+
+function deepestChild()
+{
+  var selector = document.getElementById('grand-node')
+  var selectorListArray = [];
+  var selectorList = selector.querySelectorAll("div");
+  selectorListArray.push(selectorList);
+  
+  while(selectorListArray.length > 0)
+  {
+    selectorList = selectorListArray.pop();
+    
+    for(let i = 0; i < selectorList.length; i++)
+    {
+      
+       selectorListArray.push(selectorList[i].querySelectorAll("div"));
+       selector = selectorList[i];
+    }
+    
+  }
+  
+  return selector;
+  
+}
